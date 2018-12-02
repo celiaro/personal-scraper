1c94714df1835ca5fc8936f4ef81d84abe2f8fe4
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-12-02 15:36:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,42 @@ app.get('/', (req, res) => {
 
 // scraper endpoint
 
-
 app.listen(port, () => {
   console.log(`app running on ${port}`);
+});
+
+app.get('/movies/:keyword', (req, res) => {
+  var keyword = req.params.keyword;
+
+  function findMovieCover(keyword) {
+    var nightmare = Nightmare({ show: true });
+
+    return nightmare
+      .goto('https://www.google.com')
+      .insert('input[title="Search"]', `movies ${keyword}`)
+      .click('input[value="Google Search"]')
+      .wait('.rISBZc.M4dUYb')
+
+      .evaluate(function() {
+        var photoDivs = document.querySelectorAll('img.rISBZc.M4dUYb');
+        var list = [].slice.call(photoDivs);
+
+        return list.map(function(div) {
+          return div.src;
+        });
+      })
+      .end()
+      .then(function (result) {
+        return result.slice(0, 4);
+      })
+      .then(function (images) {
+        res.json(images);
+      })
+      .catch(function (error) {
+        console.error('Search failed:', error);
+      });
+  }
+
+  findMovieCover(keyword);
+
 });
